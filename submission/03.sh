95d0f8273b4c9ebabc86c8e35357e67ede441bc4
csserumaga <alltechissues@gmail.com>
2026-06-28 14:42:48 +0300

    Added the response to 3rd file

--- submission/03.sh
+++ submission/03.sh
@@ -2,6 +2,5 @@
 # Add funds to the address.
 # Return only the Address
 ADD=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
-bitcoin-cli generatetoaddress 101 $ADD > /dev/null
+bitcoin-cli -regtest generatetoaddress 101 $ADD > /dev/null
 echo $ADD
-

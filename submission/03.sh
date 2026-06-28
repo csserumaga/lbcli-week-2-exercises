1bf183076d0ab6b2149c71492427353bad93172f
csserumaga <alltechissues@gmail.com>
2026-06-28 14:39:52 +0300

    Added the response to 3rd file

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Create a SegWit address.
 # Add funds to the address.
 # Return only the Address
+ADD=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32)
+bitcoin-cli generatetoaddress 101 $ADD > /dev/null
+echo $ADD
+
